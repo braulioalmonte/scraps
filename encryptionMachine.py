baefbf490fd3754ff5f9524c6ab1df322df190a1
Braulio Almonte <60824687+braulioalmonte@users.noreply.github.com>
2026-04-20 14:05:08 -0400

    added morse dictionary

--- encryptionMachine.py
+++ encryptionMachine.py
@@ -1,6 +1,59 @@
+#Inspired by https://cryptii.com/
 import flet as ft
 
 def main(page: ft.Page):
-    pass
+
+    morse_dict = {
+    'A': '.-',
+    'B': '-...',
+    'C': '-.-.',
+    'D': '-..',
+    'E': '.',
+    'F': '..-.',
+    'G': '--.',
+    'H': '....',
+    'I': '..',
+    'J': '.---',
+    'K': '-.-',
+    'L': '.-..',
+    'M': '--',
+    'N': '-.',
+    'O': '---',
+    'P': '.--.',
+    'Q': '--.-',
+    'R': '.-.',
+    'S': '...',
+    'T': '-',
+    'U': '..-',
+    'V': '...-',
+    'W': '.--',
+    'X': '-..-',
+    'Y': '-.--',
+    'Z': '--..',
+    '0': '-----',
+    '1': '.----',
+    '2': '..---',
+    '3': '...--',
+    '4': '....-',
+    '5': '.....',
+    '6': '-....',
+    '7': '--...',
+    '8': '---..',
+    '9': '----.',
+    ' ': '/'
+        }
+    #functions
+    def processMessage(e):
+
+        pass
+
+    #page setup
+    page.theme_mode = ft.ThemeMode.LIGHT
+
+    #controls
+    messageTextField = ft.TextField(hint_text="", on_change=processMessage)
+    resultText = ft.Text("")
+
+    page.add(messageTextField, resultText)
 
 ft.run(main=main)
