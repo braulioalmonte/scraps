b9a41d6aa6f622421df22917bec72861cdb56e0c
Braulio Almonte <60824687+braulioalmonte@users.noreply.github.com>
2026-04-20 13:46:14 -0400

    added title and exception handling for numbers and spaces

--- dollarWordsFlet.py
+++ dollarWordsFlet.py
@@ -9,25 +9,36 @@ def main(page: ft.Page):
         nonlocal lettersToNumbers
         wordSum = 0
         userWord = wordTextField.value.lower()
-        for letter in userWord:
-            wordSum+=lettersToNumbers[letter]
-        if wordSum == 100:
-            resultText.value = f"{userWord} is a dollar word!"
-            resultText.color = ft.Colors.GREEN
+        try:
+            for letter in userWord:
+                wordSum+=lettersToNumbers[letter]
+        except:
+            resultText.value = "No numbers or spaces allowed"
         else:
-            resultText.value = f"{userWord} is not a dollar word, it is worth {wordSum} cents"
-            resultText.color = ft.Colors.RED
+            if wordSum == 0:
+                resultText.value = ""
+            elif wordSum == 100:
+                resultText.value = f"{userWord} is a dollar word!"
+                resultText.color = ft.Colors.GREEN
+            else:
+                resultText.value = f"{userWord} is not a dollar word, it is worth {wordSum} cents"
+                resultText.color = ft.Colors.RED
 
     #page setup
     page.theme_mode = ft.ThemeMode.LIGHT
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.window.height = 500
+    page.window.width = 500
+    page.title = "Dollar Words Checker"
+
 
     #controls
     #!i'll finish this later
+    titleText = ft.Text("Dollar Words Checker", size=30)
     wordTextField = ft.TextField(hint_text="Write your word here: ", on_change=processWord)
     resultText = ft.Text(value="", size=20)
 
-    page.add(wordTextField, resultText)
+    page.add(titleText, wordTextField, resultText)
 
 ft.run(main=main)
