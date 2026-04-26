fa5366a7f7915bc62af49b8ba836e7a97ed2f26c
Braulio Almonte <60824687+braulioalmonte@users.noreply.github.com>
2026-04-25 22:52:55 -0400

    reverted last commit, I staged encryption machine changes by accident

--- stroopTestFlet.py
+++ stroopTestFlet.py
@@ -47,14 +47,16 @@ def main(page: ft.Page):
         lives+=1
         if lives <= 2:
             livesRow.controls[lives-1].icon = ft.Icons.HEART_BROKEN
-            livesRow.controls[lives-1].color = ft.Colors.RED
-            
         else:
-            gameOver()
+            resetGame()
 
-    def gameOver():
+    def resetGame():
         stroopText.value = "Color"
         stroopText.color = ft.Colors.WHITE
+        livesRow.controls = [ft.Icon(icon=ft.Icons.FAVORITE) for i in range(3)]
+        correctText.data = incorrectText.data = 0
+        correctText.value = f"Correct: {correctText.data}"
+        incorrectText.value = f"Incorrect: {incorrectText.data}"
         startButton.disabled = False
 
     def checkAnswer(e: ft.KeyboardEvent):
@@ -74,10 +76,6 @@ def main(page: ft.Page):
 
     def startGame(e):
         startButton.disabled = True
-        livesRow.controls = [ft.Icon(icon=ft.Icons.FAVORITE) for i in range(3)]
-        correctText.data = incorrectText.data = 0
-        correctText.value = f"Correct: {correctText.data}"
-        incorrectText.value = f"Incorrect: {incorrectText.data}"
         listener.focus()
         nextColor()
 
