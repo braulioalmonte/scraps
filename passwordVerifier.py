8eda133306431ff0eea5ac1f9a2e4a05da3986f1
Braulio Almonte <60824687+braulioalmonte@users.noreply.github.com>
2026-04-28 11:22:35 -0400

    started password verifier

--- passwordVerifier.py
+++ passwordVerifier.py
@@ -1,6 +1,36 @@
 import flet as ft
 
 def main(page: ft.Page):
-    pass
+    #functions
+    conditions = {"upperCase":{"text":"Password must include at least 1 uppercase letter", 
+                               "status":False},
+                  "lowerCase":{"text":"Password must include at least 1 lowercase letter", 
+                               "status":False},
+                  "numbers":{"text":"Password must include at least 1 number", 
+                               "status":False},
+                  "symbols":{"text":"Password must include at least 1 special character", 
+                               "status":False},
+                  "evenSum":{"text":"The sum of all numbers in the password must be an even number", 
+                               "status":False},
+                  }
+    
+    def checkConditions(e):
+        pass
+
+    def updateConditions():
+        pass
+
+    #page setup
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.vertical_alignment = ft.MainAxisAlignment.START
+    page.theme_mode = ft.ThemeMode.LIGHT
+    page.window.height = 500
+    page.window.width = 500
+    page.title = "Password Verifier"
+
+    #controls
+    passwordTextField = ft.TextField(hint_text="Write your password here:", on_change=checkConditions)
+
+    page.add(passwordTextField)
 
 ft.run(main=main)
